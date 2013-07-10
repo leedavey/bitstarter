c146badd7634c71ccf5ad7c819a361fecdede73f
leedavey <lee.davey@gmail.com>
2013-07-10 23:33:28 +0000

    reading a file

--- web.js
+++ web.js
@@ -4,7 +4,7 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
   var hellofile = fs.readFile("index.html");
-  response.send('Hello World 2!');
+  response.send(hellofile);
 });
 
 var port = process.env.PORT || 5000;
